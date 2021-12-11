a1c97be50e2aaa959a4b5bdf35ea492eb2cbbc6e
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-11 13:50:08 -0700

    Feat: Error messages displaying when application incomplete

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -3,9 +3,26 @@ class ApplicationsController < ApplicationController
   end
 
   def new
-  end 
+  end
+
+  def create
+    application = Application.new(application_params)
+
+    if application.save
+      redirect_to '/applications'
+    else
+      redirect_to '/applications/new'
+      flash[:alert] = "Error: #{error_message(application.errors)}"
+    end
+  end
 
   def show
     @applicant = Application.find(params[:id])
+  end
+
+  private
+
+  def application_params
+    params.permit(:name, :street_address, :city, :state, :zipcode, :description, :status)
   end
 end
