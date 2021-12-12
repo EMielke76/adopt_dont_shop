a2bb52ac4391a90759b2a9c0b669eeeee0af69a2
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-11 19:27:32 -0700

    Create tests for link from pets to new application

--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -12,9 +12,32 @@ RSpec.describe 'New Application' do
       expect(find('form')).to have_content('City')
       expect(find('form')).to have_content('State')
       expect(find('form')).to have_content('Zip code')
-      expect(find('form')).to have_content('Description')
-      expect(find('form')).to have_content('Status')
     end
+
+    it 'can complete an application if given correct data' do
+      app_1 = Application.create!(name:'Elora Mielke', street_address: '123 Fake Street', city: 'Littleton', state: 'CO', zipcode: '80120')
+      visit '/applications/new'
+
+      fill_in "Name", with: "#{app_1.name}"
+      fill_in :street_address, with: "#{app_1.street_address}"
+      fill_in "City", with: "#{app_1.city}"
+      fill_in "State", with: "#{app_1.state}"
+      fill_in :zipcode, with: "#{app_1.zipcode}"
+
+      click_on "Save"
+
+      expect(current_path).to eq("/applications/#{app_1.id+1}")
+
+      visit "/applications/#{app_1.id+1}"
+
+      expect(page).to have_content(app_1.name)
+      expect(page).to have_content(app_1.street_address)
+      expect(page).to have_content(app_1.city)
+      expect(page).to have_content(app_1.state)
+      expect(page).to have_content(app_1.zipcode)
+      expect(page).to have_content(app_1.description)
+      expect(page).to have_content(app_1.status)
+    end 
 
     it 'displays an error message when the form is not fully completed' do
 
@@ -22,24 +45,8 @@ RSpec.describe 'New Application' do
 
       click_on "Save"
 
-      expect(page).to have_content("Error: Name can't be blank, Street address can't be blank, City can't be blank, State can't be blank, Zipcode can't be blank, Description can't be blank, Status can't be blank")
+      expect(page).to have_content("Error: Name can't be blank, Street address can't be blank, City can't be blank, State can't be blank, Zipcode can't be blank")
       expect(page).to have_current_path('/applications/new')
-    end
-
-    xit 'can complete an application if given correct data' do
-
-      visit '/applications/new'
-
-      fill_in "Name", with: 'Elora Mielke'
-      fill_in "Address", with: '123 Fake Street'
-      fill_in "City", with: 'Littleton'
-      fill_in "State", with: 'CO'
-      fill_in "Zip code", with: '80120'
-      fill_in "Description", with: 'I love dogs!'
-      fill_in "Status", with: 'Pending'
-      click_on "Submit"
-
-      expect(current_path).to eq('/applications/index')
     end
   end
 end
