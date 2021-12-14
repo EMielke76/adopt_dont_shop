3280037b3e4a1f0515d12daef9d96207bc535c21
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-13 19:08:01 -0700

    Create tests for incomplete and case insensitive pet search returns.

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -8,7 +8,9 @@ RSpec.describe 'Application Show' do
   let!(:pet_1) {Pet.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true, shelter_id: shelter_1.id)}
   let!(:pet_2) {Pet.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true, shelter_id: shelter_1.id)}
   let!(:pet_3) {Pet.create!(name: 'Luna', breed: 'cattledog', age: 2, adoptable: true, shelter_id: shelter_1.id)}
-  let!(:pet_4) {Pet.create!(name: 'Cici', breed: 'boxer', age: 1, adoptable: true, shelter_id: shelter_1.id)}
+  let!(:pet_4) {Pet.create!(name: 'Lulu Dog', breed: 'cattledog', age: 2, adoptable: true, shelter_id: shelter_1.id)}
+  let!(:pet_5) {Pet.create!(name: 'Lola', breed: 'cattledog', age: 2, adoptable: true, shelter_id: shelter_1.id)}
+
 
   before :each do
     PetApplication.create!(pet: pet_1, application: application_1)
@@ -40,70 +42,100 @@ RSpec.describe 'Application Show' do
     end
   end
 
-  it 'allows for a search of pets by name' do
+  describe 'building an empty application' do
 
-    visit "/applications/#{application_2.id}"
-
-    expect(page).to have_content("Add a Pet to this Application")
-
-    within('#search-pet')do
-      fill_in(:pet_name, with: "Luna")
-      click_on("Search")
+    before :each do
+      visit "/applications/#{application_2.id}"
     end
 
-    expect(current_path).to eq("/applications/#{application_2.id}")
-    expect(page).to have_content(pet_3.name)
-  end
+    it 'allows for a search of pets by name' do
 
-  it 'provides a link to add a searched pet to an application' do
+      expect(page).to have_content("Add a Pet to this Application")
 
-    visit "/applications/#{application_2.id}"
+      within('#search-pet')do
+        fill_in(:pet_name, with: "Luna")
+        click_on("Search")
+      end
 
-    within('#search-pet')do
-      fill_in(:pet_name, with: "Luna")
-      click_on("Search")
-    end
-      expect(page).to have_content(pet_3.name)
-      expect(page).to have_content("Adopt Me!")
-
-    within('#search-pet') do
-      click_on("Adopt Me!")
-    end
-
-    expect(current_path).to eq("/applications/#{application_2.id}")
-
-    within('#pet-names') do
-      expect(page).to have_content(pet_3.name)
-    end
-  end
-
-  it 'allows an application to be submited only if 1 or more pets have been added' do
-
-    visit "/applications/#{application_2.id}"
-
-    within('#search-pet') do
-      fill_in(:pet_name, with: "Luna")
-      click_on("Search")
-      click_on("Adopt Me!")
-    end
-
-    expect(page).to have_content("What makes #{application_2.name} a good fit?")
-
-    within('#conditional-info') do
-      fill_in(:description, with: "Ready as I'll ever be!")
-      click_on("submit")
       expect(current_path).to eq("/applications/#{application_2.id}")
-      expect(page).to have_content("Pending")
+      expect(page).to have_content(pet_3.name)
     end
 
-    expect(page).to_not have_content("Add a Pet to this Application")
-    expect(page).to have_content(pet_3.name)
+    it 'provides a link to add a searched pet to an application' do
+
+      within('#search-pet')do
+        fill_in(:pet_name, with: "Luna")
+        click_on("Search")
+      end
+        expect(page).to have_content(pet_3.name)
+        expect(page).to have_content("Adopt Me!")
+
+      within('#search-pet') do
+        click_on("Adopt Me!")
+      end
+
+      expect(current_path).to eq("/applications/#{application_2.id}")
+
+      within('#pet-names') do
+        expect(page).to have_content(pet_3.name)
+      end
+    end
+
+    it 'allows an application to be submited only if 1 or more pets have been added' do
+
+      within('#search-pet') do
+        fill_in(:pet_name, with: "Luna")
+        click_on("Search")
+        click_on("Adopt Me!")
+      end
+
+      expect(page).to have_content("What makes #{application_2.name} a good fit?")
+
+      within('#conditional-info') do
+        fill_in(:description, with: "Ready as I'll ever be!")
+        click_on("submit")
+        expect(current_path).to eq("/applications/#{application_2.id}")
+        expect(page).to have_content("Pending")
+      end
+
+      expect(page).to_not have_content("Add a Pet to this Application")
+      expect(page).to have_content(pet_3.name)
+    end
   end
 
-  it 'does not allow submissions if no pets have been added' do
+  describe 'database functionality tests' do
 
-    visit "/applications/#{application_3.id}"
+    before :each do
+      visit "/applications/#{application_3.id}"
+    end
 
-    expect(page).to_not have_content("What Makes #{application_3.name} a good fit?")
-  end 
+    it 'does not allow submissions if no pets have been added' do
+
+      expect(page).to_not have_content("What Makes #{application_3.name} a good fit?")
+    end
+
+    it 'allows for partial matches of pet name searches' do
+
+      within('#search-pet') do
+        fill_in(:pet_name, with: "Lu")
+        click_on("Search")
+      end
+
+      expect(page).to have_content(pet_3.name)
+      expect(page).to have_content(pet_4.name)
+      expect(page).to_not have_content(pet_5.name)
+    end
+
+    it 'allows for case insensitive searches' do
+
+      within('#search-pet') do
+        fill_in(:pet_name, with: "lu")
+        click_on("Search")
+      end
+
+      expect(page).to have_content(pet_3.name)
+      expect(page).to have_content(pet_4.name)
+      expect(page).to_not have_content(pet_5.name)
+    end 
+  end
 end
