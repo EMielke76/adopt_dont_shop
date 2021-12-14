f7586cc0df12d1d5df5dac64a76968b0475158b0
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-13 17:44:55 -0700

    Gonna have a forest after I plant all these seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,8 @@ application_1 = Application.create!(name: "Elora Mielke", street_address: "123 F
 shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
 pet_1 = Pet.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true, shelter_id: shelter_1.id)
 pet_2 = Pet.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true, shelter_id: shelter_1.id)
+pet_3 = Pet.create!(name: 'Luna', breed: 'blue heeler', age: 2, adoptable: true, shelter_id: shelter_1.id)
+pet_4 = Pet.create!(name: 'Cici', breed: 'shorthair', age: 1, adoptable: true, shelter_id: shelter_1.id)
 
 PetApplication.create!(pet: pet_1, application: application_1)
 PetApplication.create!(pet: pet_2, application: application_1)
