ca9444b7602d6d480ddc645d7477b150ae493621
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-15 15:24:45 -0700

    Planting more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,26 @@ PetApplication.destroy_all
 Application.destroy_all
 Pet.destroy_all
 Shelter.destroy_all
-application_1 = Application.create!(name: "Elora Mielke", street_address: "123 Fake Street", city: "Littleton", state: "CO", zipcode: "80120", description: "None", status: "Pending")
+application_1 = Application.create!(name: "Elora Mielke", street_address: "123 Fake Street", city: "Littleton", state: "CO", zipcode: "80120")
+application_2 = Application.create!(name: "Blair Mielke", street_address: "123 Fake Street", city: "Littleton", state: "CO", zipcode: "80120", description: "I'm a great dog mom!", status: "Pending")
+application_3 = Application.create!(name: "Eric Mielke", street_address: "123 Fake Street", city: "Littleton", state: "CO", zipcode: "80120", description: "I'm a great dog dad!", status: "Pending")
+
 shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+shelter_2 = Shelter.create(name: 'New Hope Cattledog', city: 'Littleton, CO', foster_program: true, rank: 10)
+shelter_3 = Shelter.create(name: 'DACC', city: 'Denver, CO', foster_program: false, rank: 6)
+shelter_4 = Shelter.create(name: 'Boulder Buddies', city: 'Boulder, CO', foster_program: true, rank: 8)
+
 pet_1 = Pet.create!(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true, shelter_id: shelter_1.id)
 pet_2 = Pet.create!(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true, shelter_id: shelter_1.id)
-pet_3 = Pet.create!(name: 'Luna', breed: 'blue heeler', age: 2, adoptable: true, shelter_id: shelter_1.id)
-pet_4 = Pet.create!(name: 'Cici', breed: 'shorthair', age: 1, adoptable: true, shelter_id: shelter_1.id)
+pet_3 = Pet.create!(name: 'Luna', breed: 'blue heeler', age: 2, adoptable: true, shelter_id: shelter_2.id)
+pet_4 = Pet.create!(name: 'Cici', breed: 'boxer husky', age: 1, adoptable: true, shelter_id: shelter_2.id)
+pet_5 = Pet.create!(name: 'Otis', breed: 'shitpom', age: 1, adoptable: true, shelter_id: shelter_3.id)
+pet_6 = Pet.create!(name: 'Frankie', breed: 'frenchie', age: 1, adoptable: true, shelter_id: shelter_3.id)
+pet_7 = Pet.create!(name: 'Daisy', breed: 'blue heeler', age: 1, adoptable: true, shelter_id: shelter_4.id)
+pet_8 = Pet.create!(name: 'Yoshi', breed: 'muttt', age: 1, adoptable: true, shelter_id: shelter_4.id)
 
-PetApplication.create!(pet: pet_1, application: application_1)
-PetApplication.create!(pet: pet_2, application: application_1)
+PetApplication.create!(pet: pet_1, application: application_2)
+PetApplication.create!(pet: pet_2, application: application_2)
+
+PetApplication.create!(pet: pet_3, application: application_3)
+PetApplication.create!(pet: pet_4, application: application_3)
