8c7cf30d0e6515e0a7f40eabd14dd55515811187
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-15 15:35:23 -0700

    Feat: Shelters#show no longer displaying duplicate shelters

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -2,6 +2,6 @@ class Admin::SheltersController < ApplicationController
   def index
     @shelters = Shelter.reverse_alpha_names
 
-    @shelters_with_apps = Shelter.shelters_with_pending_apps
+    @shelters_with_apps = Shelter.shelters_with_pending_apps.uniq
   end
 end
