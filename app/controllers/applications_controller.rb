cf3712d88a73e2b88b9334b82810fad296654954
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-12-15 15:06:38 -0700

    Feat: PetApplicationsController now handling creation of new Pet Application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -21,9 +21,6 @@ class ApplicationsController < ApplicationController
 
     if params[:pet_name]
       @pets = Pet.search(params[:pet_name])
-    elsif params[:add_pet]
-      @applicant.pets << Pet.find(params[:add_pet])
-      @pets = @applicant.pets
     else
       @pets =[]
     end
--- app/controllers/pet_applications_controller.rb
+++ app/controllers/pet_applications_controller.rb
@@ -1,2 +1,10 @@
-class PetApplications < ApplicationController
+class PetApplicationsController < ApplicationController
+
+  def create
+    pet = Pet.find(params[:add_pet])
+    applicant = Application.find(params[:id])
+
+    PetApplication.create(pet: pet, application: applicant)
+    redirect_to "/applications/#{applicant.id}"
+  end
 end
